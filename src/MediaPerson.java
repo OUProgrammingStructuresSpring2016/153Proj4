bd2c832184d7abd92a57c6d0494b8a67d56b3218
Daniel Chambers <daniel.j.chambers-1@ou.edu>
2016-03-23 18:29:05 -0500

    Functional MediaPerson
    Functional MediaPerson. Need to fix toString methods before submission,
    but fit to test with.

--- src/MediaPerson.java
+++ src/MediaPerson.java
@@ -15,73 +15,64 @@ public class MediaPerson{
 	 * @param profession	The job(s) of the MediaPerson
 	 */
 	MediaPerson(String name, String profession){
-		
+		this.name = name;
+		this.profession = profession;
+		works = new ArrayList<String>();
 	}
 	/**
 	 * @return This MediaPerson's name
 	 */
 	public String getName(){
-		
+		return name;
 	}
 	/**
 	 * @param name The new name to give this MediaPerson
 	 */
 	public void setName(String name){
-		
+		this.name = name;
 	}
 	
 	/**
 	 * @return This MediaPerson's profession(s)
 	 */
 	public String getProfession(){
-		
+		return profession;
 	}
 	
 	/**
 	 * @param profession The new profession(s) to give this MediaPerson
 	 */
 	public void setProfession(String profession){
-		
+		this.profession = profession;
 	}
 	
 	/**
 	 * @return The ArrayList containing this MediaPerson's works
 	 */
 	public ArrayList<String> getWorks(){
-		
+		return works;
 	}
 	
 	/**
 	 * Adds a work to that the MediaPerson created/participated in.
 	 */
-	public void addWork(){
-		
+	public void addWork(String work){
+		works.add(work);
 	}
 	/**
 	 * @return The String representing this MediaPerson's works, as per project specifications
 	 */
 	public String worksToString(){
-		
+		//TODO: Fix according to project standards
+		return works.toString();
 	}
 	
 	/**
 	 * @return The String representing this MediaPerson, including works, as per project specifications
 	 */
 	public String toString(){
-		
+		//TODO: Fix according to project standards
+		return name + " " + profession + " " + worksToString();
 	}
 	
-	public ArrayList<Media> getActing()
-	{
-		
-	}
-	public ArrayList<Media> getDirecting()
-	{
-		
-	}
-	public ArrayList<Media> getProducing()
-	{
-		
-	}
-	}
 }
