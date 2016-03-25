0ba8159e4a89b793585209dc365a294f055a5a48
Daniel Chambers <daniel.j.chambers-1@ou.edu>
2016-03-24 20:34:01 -0500

    Added counters to MediaPerson
    Added counters to MediaPerson to allow easier counting of number of
    works this media person has acted/directed/produced of each type.

--- src/MediaPerson.java
+++ src/MediaPerson.java
@@ -9,6 +9,8 @@ public class MediaPerson{
 	
 	private ArrayList<String> works;
 	
+	private int moviesActed, moviesDirected, moviesProduced, seriesActed, seriesDirected, seriesProduced = 0;
+	
 	/**
 	 * Constructs a new instance of MediaPerson.
 	 * @param name	The name to give the MediaPerson
@@ -55,10 +57,54 @@ public class MediaPerson{
 	
 	/**
 	 * Adds a work to that the MediaPerson created/participated in.
+	 * Also tallies the number of works and what role the person has.
 	 */
-	public void addWork(String work){
+	public void addWork(String work, String typeOfWork, String role){	
+		
+		switch(typeOfWork){
+		case "movie":
+			switch(role){
+			case "actor": moviesActed++; break;
+			case "director": moviesDirected++; break;
+			case "producer": moviesProduced++; break;
+			default: System.out.println("ERROR: check MediaPerson method addWork() and ensure you use correct formatting."); break;
+			}
+		case "series":
+			switch(role){
+			case "actor": seriesActed++; break;
+			case "director": seriesDirected++; break;
+			case "producer": seriesProduced++; break;
+			default: System.out.println("ERROR: check MediaPerson method addWork() and ensure you use correct formatting."); break;
+			}
+		}
+			
 		works.add(work);
 	}
+	
+	public int getNumMoviesActed(){
+		return moviesActed;
+	}
+	
+	public int getNumMoviesDirected(){
+		return moviesDirected;
+	}
+	
+	public int getNumMoviesProduced(){
+		return moviesProduced;
+	}
+	
+	public int getNumSeriesActed(){
+		return seriesActed;
+	}
+	
+	public int getNumSeriesDirected(){
+		return seriesDirected;
+	}
+	
+	public int getNumSeriesProduced(){
+		return seriesProduced;
+	}
+	
 	/**
 	 * @return The String representing this MediaPerson's works, as per project specifications
 	 */
