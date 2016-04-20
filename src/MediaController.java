2700e1add55501d3f045337732992a34a3db7114
Madrane <anouar.m.madrane-1@ou.edu>
2016-04-20 16:19:36 -0500

    Update MediaController.java

--- src/MediaController.java
+++ src/MediaController.java
@@ -1,87 +1,104 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
 public class MediaController {
  
-	private View DatabaseView;
+	private View ViewMDB;
 	private Model DatabaseModel;
  
- public MediaController(MediaView DatabaseView, MediaModel DatabaseModel){
-	 this.DatabaseView = DatabaseView;
+ public MediaController(MediaView ViewMDB, MediaModel DatabaseModel){
+	 this.ViewMDB = DatabaseView;
 	 this.DatabaseModel = DatabaseModel;
-	 this.DatabaseView.addSelectionListener(new SelectionListener());
-	 this.DatabaseView.addPieChartListener(new PiechartListener());
-	 this.DatabaseView.addHistogramListener(new HistogramListener());
+	 this.ViewMDB.addSelectionListener(new SelectionListener());
+	 this.ViewMDB.addPieChartListener(new PieChartListener());
+	 this.ViewMDB.addHistogramListener(new HistogramListener());
+	 this.ViewMDB.addDeleteListener(new DeleteListener());
+	 this.ViewMDB.addAddListener(new AddListener());
+	 this.ViewMDB.addEditListener(new EditListener());
+	 this.ViewMDB.addClearListener(new ClearListener());
+	 this.ViewMDB.addExportListener(new ExportListener());
+	 this.ViewMDB.addImportListener(new ImportListener());
+	 this.ViewMDB.addLoadListener(new LoadListener());
+	 this.ViewMDB.addSaveListener(new SaveListener());
+	 
 	 
  }
  class SelectionListener implements ActionListener{
-	 public void actionPerformed(ActionEvent ){
+	 public void actionPerformed(ActionEvent arg0){
 	 	 
 	 }
 	 
  }
  class PieChartListener implements ActionListener{
-	 public void actionPerformed(ActionEvent ){
+	 public void actionPerformed(ActionEvent arg0){
 		 
 	 }
 	 
  }
  class HistogramListener implements ActionListener{
-	 public void actionPerformed(ActionEvent ){
+	 public void actionPerformed(ActionEvent arg0){
 		 
 	 }
 	 
  }
  class DeleteListener implements ActionListener{
-	 public void actionPerformed(ActionEvent ){
+	 public void actionPerformed(ActionEvent arg0){
 		 
 	 }
 	 
  }
  class AddListener implements ActionListener{
-	 public void actionPerformed(ActionEvent ){
+	 public void actionPerformed(ActionEvent arg0){
 		 
 	 }
 	 
  }
  class EditListener implements ActionListener{
-	 public void actionPerformed(ActionEvent ){
+	 public void actionPerformed(ActionEvent arg0){
 		 
 	 }
 	 
  }
  class ClearListener implements ActionListener{
-	 public void actionPerformed(ActionEvent ){
+	 public void actionPerformed(ActionEvent arg0){
 		 
 	 }
 	 
  }
  class ExportListener implements ActionListener{
-	 public void actionPerformed(ActionEvent ){
+	 public void actionPerformed(ActionEvent arg0){
 		 
 	 }
 	 
  }
  class ImportListener implements ActionListener{
-	 public void actionPerformed(ActionEvent ){
+	 public void actionPerformed(ActionEvent arg0){
 		 
 	 }
 	 
  }
  class LoadListener implements ActionListener{
-	 public void actionPerformed(ActionEvent ){
+	 public void actionPerformed(ActionEvent arg0){
 		 
 	 }
 	 
  }
  class SaveListener implements ActionListener{
-	 public void actionPerformed(ActionEvent ){
+	 public void actionPerformed(ActionEvent arg0){
 		 
 	 }
 	 
  }
  public DatabaseController(){}
- public void setModel(){}
- public getModel(){}
- public void setView(){}
- public getView(){}
+ public void setModel(DatabaseModel model){
+	 this.model = model;
+ }
+
+ public void setView(ViewMDB View){
+	 this.ViewMDB = view;
+ }
  
+
  
 }
