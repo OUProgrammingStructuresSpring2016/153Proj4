c71412567bbd46db33a8adfac099f3d8370c0341
cham5490 <daniel.j.chambers-1@ou.edu>
2016-04-21 17:28:11 -0500

    Completed DatabaseModel

--- src/DatabaseModel.java
+++ src/DatabaseModel.java
@@ -1,18 +1,42 @@
-
-package src;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class DatabaseModel {
+public class DatabaseModel extends Database implements Serializable{
 	
-	//private data fields
+	private static final long serialVersionUID = -380182387196408449L;
+	
+	
+
 	//ArrayList of ActionListeners to be registered later on
 	ArrayList<ActionListener> actionListenerList; 
 	
+	public DatabaseModel(MediaDatabase mdb, MediaPersonDatabase mpdb){
+		super(mdb, mpdb);
+	}
+	
+	public DatabaseModel(String fileName) throws IOException, ClassNotFoundException{
+		super(null, null);
+		
+		FileInputStream fis = new FileInputStream(fileName);
+		ObjectInputStream ois = new ObjectInputStream(fis);
+		DatabaseModel d = (DatabaseModel) ois.readObject();
+		ois.close();
+		
+		this.mdb = d.mdb;
+		this.mpdb = d.mpdb;
+		
+	}
+	
 	//methods that belong to the DatabaseModel Class
 	/** Register an action event listener */
-	public void addActionListener(ActionListener aL){
+	public synchronized void addActionListener(ActionListener aL){
 		if (actionListenerList == null) {
 			actionListenerList = new ArrayList<ActionListener>();
 		}
@@ -20,20 +44,31 @@ public class DatabaseModel {
 	}
 	
 	/** Remove an action event listener */
-	public void removeActionListener(ActionListener aL){
+	public synchronized void removeActionListener(ActionListener aL){
 		if (actionListenerList != null && actionListenerList.contains(aL)) {
 			actionListenerList.remove(aL);
 		}
 	}
 	
-	public void processEvent(ActionEvent event){
-			if (actionListenerList == null){
+	public void processEvent(ActionEvent e){
+		ArrayList<ActionListener> list;
+		synchronized (this) {
+			if (actionListenerList == null)
 				return;
-			}
-			
-		for (int i = 0; i < actionListenerList.size(); i++) {
-			ActionListener listener = actionListenerList.get(i);
-			listener.actionPerformed(event);
+			// Do not worry about the cast warning here.
+			list = (ArrayList<ActionListener>) actionListenerList.clone();
+		}
+		for (int i = 0; i < list.size(); i++) {
+			ActionListener listener = list.get(i);
+			listener.actionPerformed(e);
 		}
 	}
+	
+	public void saveDatabase(String fileName) throws IOException{
+		FileOutputStream filer = new FileOutputStream(fileName);
+		ObjectOutputStream bw = new ObjectOutputStream(filer);
+		bw.writeObject(this);
+		bw.close();
+	}
+	
 }
