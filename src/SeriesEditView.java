bf9f9dbf7ec05e1a105f392acc85f1b8d88156d1
ItsSabi <sabi.sabi@gmail.com>
2016-04-21 19:24:13 -0500

    Update SeriesEditView.java
    Added for running years

--- src/SeriesEditView.java
+++ src/SeriesEditView.java
@@ -9,6 +9,21 @@ public class SeriesEditView extends EditView implements ActionListener {
 	public SeriesEditView(){
 		setTitle("Series Edit View");
 		
+		//JPanel to add in the Running years of a series
+		JPanel jpRY = new JPanel();
+		jpRY.setLayout(new GridLayout(0,1));
+		JLabel jlRY = new JLabel("Running Years: ");
+		JTextField tfRY = new JTextField();
+		
+		//Add the Running year label and textfield to the JPanel
+		jpRY.add(jlRY);
+		jpRY.add(tfRY);
+		
+		//add the jpRY to the frameJP
+		frameJP.add(jpRY, BorderLayout.CENTER);
+		frameJP.add(buttons, BorderLayout.SOUTH);
+		setLocation(100,200);
+		
 	}
 	
 }
