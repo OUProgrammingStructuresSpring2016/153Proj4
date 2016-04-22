33934842ff96ccf037dc24ff572d7b614ca7bc4b
ItsSabi <sabi.sabi@gmail.com>
2016-04-21 22:16:45 -0500

    Update Database.java
    javadoc-ed

--- Database.java
+++ Database.java
@@ -4,10 +4,16 @@
  * use Database.mdb.method() 
  */
 public class Database {
-	
+	/** Media database that will be added to the main database */
 	public MediaDatabase mdb;
+	/** Media person database that will be added to the main database */
 	public MediaPersonDatabase mpdb;
 	
+	/**Constructor for the media database
+	 * 
+	 *@param mdb Media database to use
+	 *@param mpdb Media person database to use 
+	 */ 
 	public Database(MediaDatabase mdb, MediaPersonDatabase mpdb){
 		this.mdb = mdb;
 		this.mpdb = mpdb;
