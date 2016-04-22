018d8e9bb50376d5d1714d7a74e9d13ffb182810
ItsSabi <sabi.sabi@gmail.com>
2016-04-21 20:39:59 -0500

    Update MovieEditView.java

--- src/MovieEditView.java
+++ src/MovieEditView.java
@@ -1,12 +1,11 @@
 import java.awt.event.ActionListener;
 	
 /**Edit view for Movie entries when user picks Edit option
-	 * to edit an entry
-	  */
+ * to edit an entry
+*/
 public class MovieEditView extends EditView  implements ActionListener{
 
-
-	
+	/** Constructor for Movie Edit view */ 
 	public MovieEditView(){
 		setTitle("Movie Edit View");
 		
