c229a692aff1201cd2d51e86e5032c58cde66a14
ItsSabi <sabi.sabi@gmail.com>
2016-04-21 19:59:35 -0500

    Update ViewMDB.java
    Finished java doc

--- src/ViewMDB.java
+++ src/ViewMDB.java
@@ -1,11 +1,9 @@
 package src;
 
 import javax.swing.JFrame;
-
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.FlowLayout;
-
 import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -25,24 +23,34 @@ import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 import javax.swing.JToolBar;
 
+/** 
+ * View for the Media Database 
+ * /
 public class ViewMDB extends JFrame{
 
-
+/** Frame that will hold everything in the database */
 private	JFrame frame = new JFrame ("MDB");	
+
+/**Menu bar that will hold the different menu items */
 private	JMenuBar mediaMB = new JMenuBar();
 
+/**  Label for the media type JPanel that will hold the buttons */
 private	JLabel mediaType = new JLabel (" Media Type ");
+
+/** Label for the JList that will display the selected media/maker */
 private	JLabel selection = new JLabel(" Selection");
+
+/** Jlist that will display the selected Media type selected */
 private	JList  mediaOutput = new JList(); 
 
-//add the mediaType label and the radio buttons to this panel
+/** JPanel that will hold the mediaType label and the radio buttons */
 private JPanel mediaTypeJP = new JPanel(); 
 
-//add the Selection label and MediaOutput  JList to selectPanel 
+/** Selection label and MediaOutput  JList will be held in this panel */
 private JPanel selectPanel = new JPanel ();
 
 	
-	/**Radio buttons for the GUI */
+	/** Radio buttons for the GUI   Should be self explanatory*/
 private	JRadioButton media = new JRadioButton("Media");
 private	JRadioButton movies = new JRadioButton("Movies");
 private	JRadioButton series = new JRadioButton("Series");
@@ -58,10 +66,7 @@ private	JMenuItem load = new JMenuItem("Load");
 private	JMenuItem save = new JMenuItem("Save");
 private	JMenuItem exit = new JMenuItem("Exit");
 	
-	
-	
 	/** Components for the Edit menu */
-	
 private	JMenu editMenu = new JMenu("Edit ");
 private	JMenuItem add = new JMenuItem("Add");
 private	JMenuItem edit = new JMenuItem("Edit");
@@ -69,16 +74,13 @@ private	JMenuItem delete = new JMenuItem("Delete");
 private	JMenuItem clear  = new JMenuItem("Clear");
 private	JMenuItem clearALL = new JMenuItem("Clear All");
 	
-	
-	
 	/** Components for Display Menu */
-	
 private	JMenu displayMenu = new JMenu ("Display");
 private	JMenuItem pieChart = new JMenuItem("Pie Chart");
 private	JMenuItem histogram = new JMenuItem("Histrogram");
 	
 	
-	
+	/**Creates the GUI for the  Media Database View */
 	private void makeGUI(){
 		//Add selection list and MediaOutput the selectPanel 
 		selectPanel.setLayout(new GridLayout(2,4));
@@ -121,7 +123,7 @@ private	JMenuItem histogram = new JMenuItem("Histrogram");
 		frame.setLayout(new GridLayout(0,1));
 		frame.add(mediaMB);
 		
-		
+		//Add menu items to the menu bar
 		mediaMB.add(file);
 		mediaMB.add(editMenu);
 		mediaMB.add(displayMenu);
@@ -138,7 +140,5 @@ private	JMenuItem histogram = new JMenuItem("Histrogram");
 		frame.setVisible(true);
 		
 	}
-	
-	
 	
 }
