4519763d66153c39b3f17d6d714a7c83f59e3f0c
ItsSabi <sabi.sabi@gmail.com>
2016-04-21 20:46:37 -0500

    Update MediaMakersEntryView.java

--- src/MediaMakersEntryView.java
+++ src/MediaMakersEntryView.java
@@ -1,10 +1,12 @@
 package hereAretheENTRYVIEWS;
 
 import java.awt.event.ActionListener;
-
+/**
+ * The view for the Media Makers when someone hits ADD and has a media maker selected (actor,director,producer)
+ */
 public class MediaMakersEntryView extends EntryView implements ActionListener {
 
-	
+	/**Constructor for this class */
 		public MediaMakersEntryView (){
 			lblTitle.setText("Name: ");
 			jlYear.setText("Profession: ");
